39fb924d599c3c2adb33b2319e7777c8603186e6
fpignataro <>
2021-02-19 10:41:14 -0300

    docker-composer path

--- deploy/common-files/install_docker.sh
+++ deploy/common-files/install_docker.sh
@@ -16,6 +16,5 @@ systemctl enable containerd.service
 systemctl start docker.service
 systemctl start containerd.service
 
-curl -L "https://github.com/docker/compose/releases/download/1.28.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
-ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+curl -L "https://github.com/docker/compose/releases/download/1.28.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/bin/docker-compose
+chmod +x /usr/bin/docker-compose
